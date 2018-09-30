5cfffeb5e5a8298dfe364da1db831db712dffb10
Dmitry Logvinenko <5diezfun@gmail.com>
2018-09-30 11:36:44 +0300

    Add MortgageCalculator (js)
    Add base index.html.
    Add base stylesheet.
    Add simple webserver.

---
+++ gppl/js/mortgage.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var name = "mortgage";
+var mortgage = function(root){
+    var valueNode = root.querySelector('.mortgage__value');
+    var rateNode = root.querySelector('.mortgage__rate');
+    var termNode = root.querySelector('.mortgage__term');
+    var overpaymentsNode = root.querySelector('.mortgage__overpayments');
+
+    var recalc = function(value, rate, term){
+        return value * rate * term
+    };
+
+    var update = function(){
+        overpaymentsNode.value = recalc(valueNode.value, rateNode.value, termNode.value)
+    };
+
+    root.addEventListener("change", update);
+
+    update();
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    mortgage(document.querySelector("." + name))
+});
